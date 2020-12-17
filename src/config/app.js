3f776b5563db307b0fe56a184b05a53d7d7c8748
Crowdbotics <team@crowdbotics.com>
2020-12-17 13:23:26 +0000

    Initial commit.

---
+++ src/config/app.js
@@ -0,0 +1,7 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://testing4-4987-dev-16863.botics.co",
+  defaultTimeout: 5000,
+  pubPublishKey: 'pub-c-09038c51-029c-47a7-95f2-b6c7407afb12',
+  pubSubscribeKey: 'sub-c-6b1bee04-ba9f-11e9-8753-ce76e7dc5905'
+};
